b216fb865b12b2715dc8f7bc599acbc97bfc2e91
Pedro do PHP <pedrofalck2000@gmail.com>
2025-09-14 18:03:03 -0300

    remover cpf

--- models/user.go
+++ models/user.go
@@ -7,7 +7,6 @@ import (
 type User struct {
 	gorm.Model
 	NomeCompleto  string `json:"nome_completo" gorm:"not null"`
-	CPF           string `json:"cpf" gorm:"unique;not null"`
 	Email         string `json:"email" gorm:"unique;not null"`
 	Whatsapp      string `json:"whatsapp" gorm:"not null"`
 	FirabaseToken string `json:"firebase_token" gorm:"null"`
